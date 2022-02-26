7e670e7bd803de6587c98a81e16548539c0664e5
Victor Dolsan <victorddf5@gmail.com>
2022-02-25 23:30:27 -0300

    Enviando arquivos

--- CleanArchMvc.WebUI/Startup.cs
+++ CleanArchMvc.WebUI/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CleanArchMvc.Infra.IoC;
 
 namespace CleanArchMvc.WebUI
 {
@@ -23,6 +24,7 @@ namespace CleanArchMvc.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddInfrastructure(Configuration);
             services.AddControllersWithViews();
         }
 
